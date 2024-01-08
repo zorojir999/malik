420c1d6b0c835f0a58122cbb4f365eb33fe28926
zorojir999 <zorojir999@gmail.com>
2024-01-08 17:26:15 +0600

    zoro

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 from os import getenv
 
-BOT_TOKEN = '6770894028:AAFOSGU-gfUfDsyfdAyDocCWpIiJTaIbAbA'
+
 load_dotenv()
 bot = Bot(token=getenv('BOT_TOKEN'))
 dp = Dispatcher()
